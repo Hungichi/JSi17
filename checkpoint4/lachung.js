e2aab38249867ec89201f0d7c09e2a55331d7fcf
Hungichi <117996477+Hungichi@users.noreply.github.com>
2022-12-09 20:32:46 +0700

    Add files via upload

---
+++ checkpoint4/lachung.js
@@ -0,0 +1,12 @@
+
+for(let a=4; a<=20;a++){
+    if(a % 2 == 0){
+        let number = a
+        console.log(number)
+    }
+};
+for( let i= 0;i <6 ; i++){
+    for(let j= 0 ; j< i; j++){
+        
+    }
+}
